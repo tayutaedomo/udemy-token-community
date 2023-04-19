911bf93296bbf7f37faf1cf926b0ecb1454a7a4d
tayutaedomo <tayutaedomo@gmail.com>
2023-04-19 21:26:50 +0900

    MemberNFT: NFT 作成のテストを追加

--- test/MemberNFT.ts
+++ test/MemberNFT.ts
@@ -1,18 +1,21 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { Contract, ContractFactory } from "ethers";
+import { Contract, ContractFactory, Signer } from "ethers";
 import { ethers } from "hardhat";
 
 describe("MemberNFT コントラクト", function () {
   const name = "MemberNFT";
   const symbol = "MEM";
+  const tokenURI1 = "hoge1";
+  const tokenURI2 = "hoge2";
 
   let MemberNFT: ContractFactory;
   let memberNFT: Contract;
   let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
 
   beforeEach(async function () {
-    [owner] = await ethers.getSigners();
+    [owner, addr1] = await ethers.getSigners();
     MemberNFT = await ethers.getContractFactory("MemberNFT");
     memberNFT = await MemberNFT.deploy();
     await memberNFT.deployed();
@@ -25,5 +28,23 @@ describe("MemberNFT コントラクト", function () {
 
   it("デプロイアドレスが owner にセットされるべき", async function () {
     expect(await memberNFT.owner()).to.equal(owner.address);
+  });
+
+  it("owner は NFT 作成できるべき", async function () {
+    await memberNFT.nftMint(addr1.address, tokenURI1);
+    expect(await memberNFT.ownerOf(1)).to.equal(addr1.address);
+  });
+
+  it("NFT 作成のたびに tokenId がインクリメントされるべき", async function () {
+    await memberNFT.nftMint(addr1.address, tokenURI1);
+    await memberNFT.nftMint(addr1.address, tokenURI2);
+    expect(await memberNFT.tokenURI(1)).to.equal(tokenURI1);
+    expect(await memberNFT.tokenURI(2)).to.equal(tokenURI2);
+  });
+
+  it("owner 以外は NFT 作成に失敗すべき", async function () {
+    await expect(
+      memberNFT.connect(addr1 as any).nftMint(addr1.address, tokenURI1)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
   });
 });
