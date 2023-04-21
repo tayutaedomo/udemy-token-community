28b003e61b635503c5790a7c3087ff9192585199
tayutaedomo <tayutaedomo@gmail.com>
2023-04-21 21:14:48 +0900

    MemberNFT: デプロイスクリプトを追加

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import "dotenv/config";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_MUMBAI_API_KEY}}`,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_API_KEY}}`,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
---
+++ scripts/memberNFTDeploy.ts
@@ -0,0 +1,52 @@
+import { ethers } from "hardhat";
+import fs from "fs";
+import "dotenv/config";
+
+const main = async () => {
+  const addr1 = process.env.WALLET_ADDRESS_1;
+
+  const tokenURIs = [1, 2, 3, 4, 5].map((value) => {
+    return `ipfs://bafybeigyod7ldrnytkzrw45gw2tjksdct6qaxnsc7jdihegpnk2kskpt7a/metadata${value}.json`;
+  });
+
+  // デプロイ
+  const MemberNFT = await ethers.getContractFactory("MemberNFT");
+  const memberNFT = await MemberNFT.deploy();
+  await memberNFT.deployed();
+
+  console.log(`Contract deployed: ${memberNFT.address}`);
+
+  // NFT を mint する
+  // const mintPromises = tokenURIs.map(
+  //   async (tokenURI: string, index: number) => {
+  //     console.log(`NFT#${index + 1}, Minting with ${addr1}, ${tokenURI}`);
+  //     const tx = await memberNFT.nftMint(addr1, tokenURI);
+  //     await tx.wait(); // トランザクションの完了を待つ
+  //     console.log(`NFT#${index + 1} minted.`);
+  //   }
+  // );
+  // await Promise.all(mintPromises);
+  // API コールするため all ではなく for を使って直列に処理する
+  for (const [index, tokenURI] of tokenURIs.entries()) {
+    console.log(`NFT#${index + 1}, Minting with ${addr1}, ${tokenURI}`);
+    const tx = await memberNFT.nftMint(addr1, tokenURI);
+    await tx.wait(); // トランザクションの完了を待つ
+    console.log(`NFT#${index + 1} minted...`);
+  }
+
+  fs.writeFileSync("./.env", `MEMBER_NFT_ADDRESS="${memberNFT.address}"\n`, {
+    flag: "a",
+  });
+};
+
+const memberNFTDeploy = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+memberNFTDeploy();
