f071b04b8d09397f2eefd93f90c67fd37bdb8cfd
tayutaedomo <tayutaedomo@gmail.com>
2023-04-22 19:23:02 +0900

    MemberNFT: デプロイのコードを修正

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,19 +2,22 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "dotenv/config";
 
+const { SEPOLIA_API_URL, MUMBAI_API_URL, PRIVATE_KEY } = process.env;
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  defaultNetwork: "sepolia",
   networks: {
-    mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_MUMBAI_API_KEY}}`,
-      accounts: [process.env.PRIVATE_KEY!],
-    },
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_API_KEY}}`,
-      accounts: [process.env.PRIVATE_KEY!],
+      url: SEPOLIA_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    mumbai: {
+      url: MUMBAI_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
     },
   },
 };
--- scripts/memberNFTDeploy.ts
+++ scripts/memberNFTDeploy.ts
@@ -2,10 +2,12 @@ import { ethers } from "hardhat";
 import fs from "fs";
 import "dotenv/config";
 
-const main = async () => {
-  const addr1 = process.env.WALLET_ADDRESS_1;
+const { WALLET_ADDRESS_1, WALLET_ADDRESS_2 } = process.env;
 
-  const tokenURIs = [1, 2, 3, 4, 5].map((value) => {
+const main = async () => {
+  const addr1 = WALLET_ADDRESS_1;
+
+  const tokenURIs = [1, 2, 3, 4].map((value) => {
     return `ipfs://bafybeigyod7ldrnytkzrw45gw2tjksdct6qaxnsc7jdihegpnk2kskpt7a/metadata${value}.json`;
   });
 
@@ -28,8 +30,9 @@ const main = async () => {
   // await Promise.all(mintPromises);
   // API コールするため all ではなく for を使って直列に処理する
   for (const [index, tokenURI] of tokenURIs.entries()) {
-    console.log(`NFT#${index + 1}, Minting with ${addr1}, ${tokenURI}`);
-    const tx = await memberNFT.nftMint(addr1, tokenURI);
+    const addr = index % 2 === 0 ? WALLET_ADDRESS_1 : WALLET_ADDRESS_2;
+    console.log(`NFT#${index + 1}, Minting to:${addr}, uri:${tokenURI}`);
+    const tx = await memberNFT.nftMint(addr, tokenURI);
     await tx.wait(); // トランザクションの完了を待つ
     console.log(`NFT#${index + 1} minted...`);
   }
