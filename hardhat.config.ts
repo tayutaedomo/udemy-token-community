c3f5eb5e8068b5bee1f619a47cbc9b5258ec6c92
tayutaedomo <tayutaedomo@gmail.com>
2023-04-22 19:53:15 +0900

    MemberNFT: verify のためのコードを追加

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
 import "dotenv/config";
 
-const { SEPOLIA_API_URL, MUMBAI_API_URL, PRIVATE_KEY } = process.env;
+const {
+  SEPOLIA_API_URL,
+  MUMBAI_API_URL,
+  PRIVATE_KEY,
+  // ETHERSCAN_API_KEY,
+  POLYGONSCAN_API_KEY,
+} = process.env;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -19,6 +26,10 @@ const config: HardhatUserConfig = {
       url: MUMBAI_API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
+  },
+  etherscan: {
+    // apiKey: ETHERSCAN_API_KEY,
+    apiKey: POLYGONSCAN_API_KEY,
   },
 };
 
