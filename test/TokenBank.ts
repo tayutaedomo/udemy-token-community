0073a19fde743d89a6cea41372abf5f06e28b748
tayutaedomo <tayutaedomo@gmail.com>
2023-04-24 20:53:06 +0900

    TokenBank: トークン引出機能を追加

--- test/TokenBank.ts
+++ test/TokenBank.ts
@@ -114,5 +114,38 @@ describe("TokenBank コントラクト", function () {
         .emit(tokenBank, "TokenDeposit")
         .withArgs(addr1.address, 100);
     });
+
+    it("トークン引き出しが実行できるべき", async function () {
+      const startBankBalance = await tokenBank
+        .connect(addr1 as any)
+        .bankBalanceOf(addr1.address);
+      const startTotalBankBalance = await tokenBank
+        .connect(addr1 as any)
+        .bankTotalDeposit();
+
+      await tokenBank.connect(addr1 as any).withdraw(100);
+
+      const endBankBalance = await tokenBank
+        .connect(addr1 as any)
+        .bankBalanceOf(addr1.address);
+      const endTotalBankBalance = await tokenBank
+        .connect(addr1 as any)
+        .bankTotalDeposit();
+
+      expect(endBankBalance).to.equal(startBankBalance.sub(100));
+      expect(endTotalBankBalance).to.equal(startTotalBankBalance.sub(100));
+    });
+
+    it("預入トークンが不足していた場合、引き出しに失敗すべき", async function () {
+      await expect(
+        tokenBank.connect(addr1 as any).withdraw(101)
+      ).to.revertedWith("An amount greater than your tokenBank balance!");
+    });
+
+    it("引き出し後には TokenWithdraw イベントが発行されるべき", async function () {
+      await expect(tokenBank.connect(addr1 as any).withdraw(100))
+        .emit(tokenBank, "TokenWithdraw")
+        .withArgs(addr1.address, 100);
+    });
   });
 });
