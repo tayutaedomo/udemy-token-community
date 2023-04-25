fa7189a4f168c32fe411828b0221aa015f68a793
tayutaedomo <tayutaedomo@gmail.com>
2023-04-25 18:08:01 +0900

    frontend: 各種インポート

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    MEMBER_NFT_ADDRESS: process.env.MEMBER_NFT_ADDRESS,
+    TOKEN_BANK_ADDRESS: process.env.TOKEN_BANK_ADDRESS,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
